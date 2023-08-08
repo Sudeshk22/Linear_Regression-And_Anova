aee7b36797806c72f1023197437e6ace957280ca
sudesh kumari <110173175+Sudeshk22@users.noreply.github.com>
2023-08-08 18:17:16 +0530

    Add files via upload

---
+++ Labs_solutions/lab-2.R
@@ -0,0 +1,45 @@
+library(readxl)
+#nstall.packages("openxlsx")
+getwd()
+my_dir <- "/users/math/msc/sudeshk22/Downloads"
+setwd(my_dir)
+library(openxlsx)
+#file_path <- "/users/math/msc/sudeshk22/Downloads/RocketData"
+#Problem 1
+rocket_D <- read_excel("RocketData.xlsx")
+getwd()
+View(rocket_D)
+Y<- rocket_D$Y
+x <- rocket_D$X
+
+# fitting the linear model 
+model1 <- lm(Y~x)
+summary(model1)
+
+#problem2
+TimeDelivery_D <- read_excel("TimeDeliveryData.xlsx")
+Y <- TimeDelivery_D$Y
+x1 <- TimeDelivery_D$X1
+x2 <- TimeDelivery_D$X2
+
+#fitting the linear model 
+model2 <- lm(Y~ x1+x2)
+summary(model2)
+
+#problem 3
+x <- cbind(1,rocket_D$X)
+Y1 <- rocket_D$Y
+beta_hat1 <- c( 2627.822, -37.154)
+y_pred1 <- x%*% beta_hat1
+SSE1 <- t(Y1-y_pred1)%*% (Y1-y_pred1)
+# unbiased estimator of sigma 
+u_est_sigma <- SSE1/(20-2)
+print(u_est_sigma)
+
+sigma_mat <- SSE1[1,1]*(t(x)%*%x)
+
+#b) part 
+beta_1_hat <- 2627.822     
+beta_2_hat <- -37.154 
+t1 <- beta_1_hat/sqrt(sigma_mat[1,1])
+t2 <- beta_2_hat/sqrt(sigma_mat[2,2])
